f3f4e4a1fb42ea854cd35f0b46c384579a59c942
jamias0jeffrey <jamias0jeffrey@gmail.com>
2018-06-16 00:12:04 +0800

    fix

--- index.php
+++ index.php
@@ -25,13 +25,67 @@
     ?>
 
 	<main class="wrapperIndex">
-		
-		<h1>Index Page</h1>
+
+			
+		<header class="main-header" role="banner">
+		  <img src="./assets/img/banner.jpg" alt="Banner Image"/>
+		</header>
+
+		<div class="container" id="boxes">
+			
+			<div class="row">
+
+				<div class="col-md-3 col-sm-6 char_col">
+					<div class="char_item d-flex flex-row align-items-center justify-content-start">
+						<div class="char_icon"><i class="fas fa-truck"></i></div>
+						<div class="char_content">
+							<div><strong>Free Delivery</strong></div>
+							<div class="char_subtitle">Within Manila</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 char_col">
+					<div class="char_item d-flex flex-row align-items-center justify-content-start">
+						<div class="char_icon"><i class="fas fa-truck"></i></div>
+						<div class="char_content">
+							<div><strong>Free Delivery</strong></div>
+							<div class="char_subtitle">Within Manila</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 char_col">
+					<div class="char_item d-flex flex-row align-items-center justify-content-start">
+						<div class="char_icon"><i class="fas fa-truck"></i></div>
+						<div class="char_content">
+							<div><strong>Free Delivery</strong></div>
+							<div class="char_subtitle">Within Manila</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 char_col">
+					<div class="char_item d-flex flex-row align-items-center justify-content-start">
+						<div class="char_icon"><i class="fas fa-truck"></i></div>
+						<div class="char_content">
+							<div><strong>Free Delivery</strong></div>
+							<div class="char_subtitle">Within Manila</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+		</div>
+
+	
 
 
 	</main> <!-- End of wrapper -->
 
-
+<?php require_once "./partials/footer.php" ?>
 
 <?php
 	require_once "./partials/tail.php"
